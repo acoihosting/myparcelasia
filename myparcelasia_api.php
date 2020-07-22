cffa75373c316a4062762f293e97c5fa9263ca76
acoihosting <68663924+acoihosting@users.noreply.github.com>
2020-07-23 07:28:19 +0800

    Create myparcelasia_api.php

---
+++ myparcelasia_api.php
@@ -0,0 +1,187 @@
+<?php 
+if ( ! class_exists( 'Myparcelasia_Shipping_API' ) ) {
+    class Myparcelasia_Shipping_API {
+
+        {
+	const DEMO_URL = 'http://mpa-demo.ap-southeast-1.elasticbeanstalk.com/apiv2/';
+	const LIVE_URL = 'https://app.myparcelasia.com/apiv2/';
+
+	protected $api_key;
+	protected $secret_key;
+	protected $url;
+	protected $use_ssl = true;
+	protected $last_error;
+
+	public function __construct($api_key, $secret_key)
+	{
+		$this->api_key = $api_key;
+		$this->secret_key = $secret_key;
+		$this->url = self::LIVE_URL;
+	}
+
+	public function useDemo($flag = true)
+	{
+		$this->url = $flag ? self::DEMO_URL : self::LIVE_URL;
+	}
+
+	public function useSsl($flag = true)
+	{
+		$this->use_ssl = $flag;
+	}
+
+	public function getLastError()
+	{
+		return $this->last_error;
+	}
+
+	protected function logError($function, $request, $response)
+	{
+		$this->last_error = [
+			'function' => $function,
+			'request' => $request,
+			'response' => $response
+		];
+
+		$error_message = 'MyParcelAsia Error:' . PHP_EOL;
+		$error_message .= 'function: ' . $function . PHP_EOL;
+		$error_message .= 'request: ' . PHP_EOL;
+		$error_message .= '-> url: ' . $request['url'] . PHP_EOL;
+		$error_message .= '-> data: ' . json_encode($request['data']) . PHP_EOL;
+		$error_message .= 'response: ' . PHP_EOL;
+		$error_message .= '-> http_code: ' . $response['http_code'] . PHP_EOL;
+		$error_message .= '-> body: ' . $response['body'] . PHP_EOL;
+
+		error_log($error_message);
+	}
+
+	protected function snakeCase($value)
+	{
+		return strtolower(preg_replace('/(?<!^)([A-Z])/', '_' . '$1', $value));
+	}
+
+	protected function curlInit()
+	{
+		$this->last_error = null;
+
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+
+		if ($this->use_ssl === false) {
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+		}
+
+		return $ch;
+	}
+
+	protected function curlPostRequest($function, $data = [], $return_raw_data = false)
+	{
+		$action = $this->snakeCase($function);
+		$url = $this->url . $action;
+
+		$data = array_merge([
+			'api_key' => $this->api_key
+		], $data);
+
+		$ch = $this->curlInit();
+
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+		curl_setopt($ch, CURLOPT_URL, $url);
+
+		$body = curl_exec($ch);
+		$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+		curl_close($ch);
+
+		if ($return_raw_data) return $body;
+
+		$decoded_body = json_decode($body, true);
+
+		if ($http_code !== 200 || json_last_error() !== JSON_ERROR_NONE) {
+			$this->logError(
+				$function,
+				compact('url', 'data'),
+				compact('http_code', 'body')
+			);
+
+			return null;
+		}
+
+		return $decoded_body;
+	}
+
+	public function user()
+	{
+		return $this->curlPostRequest(__FUNCTION__);
+	}
+
+	public function getPostcodeDetails($postcode)
+	{
+		return $this->curlPostRequest(__FUNCTION__, ['postcode' => $postcode]);
+	}
+
+	public function checkPrice($data)
+	{
+		return $this->curlPostRequest(__FUNCTION__, $data);
+	}
+
+	public function getParcelSizes()
+	{
+		return $this->curlPostRequest(__FUNCTION__);
+	}
+
+	public function getContentTypes()
+	{
+		return $this->curlPostRequest(__FUNCTION__);
+	}
+
+	public function createShipment($data)
+	{
+		return $this->curlPostRequest(__FUNCTION__, $data);
+	}
+
+	public function getCartItems()
+	{
+		return $this->curlPostRequest(__FUNCTION__);
+	}
+
+	public function checkout($shipment_keys)
+	{
+		return $this->curlPostRequest(__FUNCTION__, ['shipment_keys' => $shipment_keys]);
+	}
+
+	public function getShipmentStatuses()
+	{
+		return $this->curlPostRequest(__FUNCTION__);
+	}
+
+	public function getShipments($shipment_keys)
+	{
+		return $this->curlPostRequest(__FUNCTION__, ['shipment_keys' => $shipment_keys]);
+	}
+
+	public function getShipmentHistory($page = 1)
+	{
+		return $this->curlPostRequest(__FUNCTION__, ['page' => $page]);
+	}
+
+	public function getConsignmentNote($data)
+	{
+		return $this->curlPostRequest(__FUNCTION__, $data, true);
+	}
+
+	public function checkPriceBulk($data)
+	{
+		return $this->curlPostRequest(__FUNCTION__, $data);
+	}
+
+	public function createBulkAwb($data)
+	{
+		return $this->curlPostRequest(__FUNCTION__, $data);
+	}
+}
+?>
